19067ae04c79b538c1278081f6dfee2e69bbac1a
Katharina <k.kaiser1@hotmail.com>
2013-05-05 23:19:33 +0200

    new url changed settings

--- Obstbaumapp/src/jku/itprojekt/obstbaumapp/MainActivity.java
+++ Obstbaumapp/src/jku/itprojekt/obstbaumapp/MainActivity.java
@@ -2,17 +2,11 @@ package jku.itprojekt.obstbaumapp;
 
 
 import android.os.Bundle;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.view.Menu;
-import android.view.View;
-import android.webkit.GeolocationPermissions;
-import android.webkit.WebChromeClient;
 import android.webkit.WebView;
-import android.webkit.WebViewClient;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.ProgressBar;
 
 public class MainActivity extends Activity {
 
@@ -28,34 +22,16 @@ public class MainActivity extends Activity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		//getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
 	
 	public void setSettings() {
 
 		mWebView = (WebView) findViewById(R.id.webView1);
-		// Banner
-		
-		// Zoom deaktivieren
-		//mWebView.getSettings().setSupportZoom(false);
-		//mWebView.getSettings().setBuiltInZoomControls(false);
-
-		// set User-Agent with package-name & versionname
-		try {
-			mWebView.getSettings().setUserAgentString(
-					getPackageName()
-							+ " "
-							+ getPackageManager().getPackageInfo(
-									getPackageName(), 0).versionName);
-		} catch (NameNotFoundException e) {
-
-			e.printStackTrace();
-		}
 		// allow Javascript and Geolocation
 		mWebView.getSettings().setJavaScriptEnabled(true);
 		mWebView.getSettings().setGeolocationEnabled(true);
-		// add a new JavaspriptInterface.
 	}
 
 	/**
@@ -64,7 +40,7 @@ public class MainActivity extends Activity {
 	public void startBrowser() {
 
 		setSettings();
-		mWebView.loadUrl("http://go.yuri.at/obst/");
+		mWebView.loadUrl("http://obst.linzwiki.at/");
 
 	}
 
