bf41eaf2ceda0bba009773ae3753643dc2c62d83
Katharina <k.kaiser1@hotmail.com>
2013-05-07 20:31:11 +0200

    added menu

--- Obstbaumapp/src/jku/itprojekt/obstbaumapp/MainActivity.java
+++ Obstbaumapp/src/jku/itprojekt/obstbaumapp/MainActivity.java
@@ -1,6 +1,5 @@
 package jku.itprojekt.obstbaumapp;
 
-
 import android.os.Bundle;
 import android.annotation.SuppressLint;
 import android.app.Activity;
@@ -8,6 +7,7 @@ import android.content.Context;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.graphics.Bitmap;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -17,11 +17,11 @@ import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
-	
 	WebView mWebView; // Webview which handles the main app
 	TextView txterror;
 	Button btnerror;
 	String url;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -36,7 +36,23 @@ public class MainActivity extends Activity {
 		url = "http://linz.pflueckt.at/";
 		return true;
 	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.it_neuLaden:
+			reloadPage();
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+
+	}
 	
+	public void reloadPage(){
+		mWebView.loadUrl("http://obst.linzwiki.at/");
+	}
+
 	public void setSettings() {
 
 		btnerror = (Button) findViewById(R.id.button1);
@@ -47,26 +63,27 @@ public class MainActivity extends Activity {
 		// allow Javascript and Geolocation
 		mWebView.getSettings().setJavaScriptEnabled(true);
 		mWebView.getSettings().setGeolocationEnabled(true);
-		
-		
+
 		mWebView.setWebViewClient(new WebViewClient() {
 
 			@Override
 			public void onReceivedError(WebView view, int errorCode,
 					String description, String failingUrl) {
-				
-				showToast(MainActivity.this, description, Toast.LENGTH_SHORT);
 
-				//view.loadData("Keine Verbindung vorhanden", "text/html","utf-8");
-				mWebView.loadData("Keine Verbindung vorhanden", "text/html","utf-8");
+				Toast.makeText(MainActivity.this, description, Toast.LENGTH_SHORT);
+				// view.loadData("Keine Verbindung vorhanden",
+				// "text/html","utf-8");
+				mWebView.loadData("Keine Verbindung vorhanden", "text/html",
+						"utf-8");
 
 				btnerror.setVisibility(View.VISIBLE);
 				btnerror.setOnClickListener(new View.OnClickListener() {
 					public void onClick(View v) {
-						mWebView.loadUrl("http://obst.linzwiki.at/");
+						reloadPage();
 						btnerror.setVisibility(View.GONE);
-						//showToast(MainActivity.this, "click", Toast.LENGTH_SHORT);
-						//((WebView) v).loadUrl("http://obst.linzwiki.at/");
+						// showToast(MainActivity.this, "click",
+						// Toast.LENGTH_SHORT);
+						// ((WebView) v).loadUrl("http://obst.linzwiki.at/");
 					}
 				});
 			}
@@ -74,19 +91,12 @@ public class MainActivity extends Activity {
 		});
 	}
 	
-	public void showToast(Context context, String text, int time)
-	{
-		Toast.makeText(this, text, time).show();
-	}
-
 	/**
 	 * starting the Browser //main input
 	 */
 	public void startBrowser() {
-
 		setSettings();
-		mWebView.loadUrl("http://obst.linzwiki.at/");
-
+		reloadPage();
 	}
 
 }
