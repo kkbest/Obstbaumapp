d7cba234e7a0a073b7b530706c1c1995db62a443
Katharina <k.kaiser1@hotmail.com>
2013-05-07 19:26:53 +0200

    added not online

--- Obstbaumapp/src/jku/itprojekt/obstbaumapp/MainActivity.java
+++ Obstbaumapp/src/jku/itprojekt/obstbaumapp/MainActivity.java
@@ -4,14 +4,23 @@ package jku.itprojekt.obstbaumapp;
 import android.os.Bundle;
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Context;
 import android.content.pm.PackageManager.NameNotFoundException;
+import android.graphics.Bitmap;
 import android.view.Menu;
+import android.view.View;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
 	
 	WebView mWebView; // Webview which handles the main app
+	TextView txterror;
+	Button btnerror;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -28,10 +37,44 @@ public class MainActivity extends Activity {
 	
 	public void setSettings() {
 
+		btnerror = (Button) findViewById(R.id.button1);
+		btnerror.setVisibility(View.GONE);
+		btnerror.setText("Reload");
+
 		mWebView = (WebView) findViewById(R.id.webView1);
 		// allow Javascript and Geolocation
 		mWebView.getSettings().setJavaScriptEnabled(true);
 		mWebView.getSettings().setGeolocationEnabled(true);
+		
+		
+		mWebView.setWebViewClient(new WebViewClient() {
+
+			@Override
+			public void onReceivedError(WebView view, int errorCode,
+					String description, String failingUrl) {
+				
+				showToast(MainActivity.this, description, Toast.LENGTH_SHORT);
+
+				//view.loadData("Keine Verbindung vorhanden", "text/html","utf-8");
+				mWebView.loadData("Keine Verbindung vorhanden", "text/html","utf-8");
+
+				btnerror.setVisibility(View.VISIBLE);
+				btnerror.setOnClickListener(new View.OnClickListener() {
+					public void onClick(View v) {
+						mWebView.loadUrl("http://obst.linzwiki.at/");
+						btnerror.setVisibility(View.GONE);
+						//showToast(MainActivity.this, "click", Toast.LENGTH_SHORT);
+						//((WebView) v).loadUrl("http://obst.linzwiki.at/");
+					}
+				});
+			}
+
+		});
+	}
+	
+	public void showToast(Context context, String text, int time)
+	{
+		Toast.makeText(this, text, time).show();
 	}
 
 	/**
